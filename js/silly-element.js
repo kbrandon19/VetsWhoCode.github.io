73a18231ee17df8df799116bc1c55d91889dafde
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2022-05-12 19:07:30 -0500

    adding remaining genre texts and functionality

--- js/silly-element.js
+++ js/silly-element.js
@@ -1,81 +1,71 @@
 //photography genre type display when hover
 
-    // various background colors
-    let defaultColor = "#ffffff";
-    let portraitColor = '#E8CDDA';
-    let autoColor = '#AF4546';
-    let productColor = '#B1AFAA';
-    let fashionColor = '#D2A97C';
-    let interiorColor = '#9F8E74';
-    
-        //grid items
-    const portraitGrid = document.getElementsByClassName('portrait');
-    const automotiveGrid = document.getElementsByClassName('automotive');
-    const productGrid = document.getElementsByClassName('product');
-    const fashionGrid = document.getElementsByClassName('fashion');
-    const interiorGrid = document.getElementsByClassName('interior');
+// various background colors
+let defaultColor = "#ffffff";
+let portraitColor = "#E8CDDA";
+let autoColor = "#AF4546";
+let productColor = "#B1AFAA";
+let fashionColor = "#D2A97C";
+let interiorColor = "#9F8E74";
 
-    
-    
-    //genre text
-    const portraitText = document.getElementById('portrait-text');
-   
-    
-    
-    //grid wrapper
-    const portfolioWrapper = document.getElementById('portfolio');
-    
+//grid items
+const portraitGrid = document.getElementsByClassName("portrait");
+const automotiveGrid = document.getElementsByClassName("automotive");
+const productGrid = document.getElementsByClassName("product");
+const fashionGrid = document.getElementsByClassName("fashion");
+const interiorGrid = document.getElementsByClassName("interior");
 
-    // color change for Portait
-    portraitGrid[0].addEventListener('mouseenter', ()=>{
-        portfolioWrapper.style.backgroundColor = portraitColor;
-        portraitText.classList.add('show');
-        
-    });
-    portraitGrid[0].addEventListener('mouseleave', ()=>{
-        portfolioWrapper.style.backgroundColor = defaultColor;
-        
-    });
+//genre text
+const portraitText = document.getElementById("portrait-text");
+const autoText = document.getElementById("auto-text");
 
-    // color change for Auto
-    automotiveGrid[0].addEventListener('mouseenter', ()=>{
-        portfolioWrapper.style.backgroundColor = autoColor;
-        
-    });
-    automotiveGrid[0].addEventListener('mouseleave', ()=>{
-        portfolioWrapper.style.backgroundColor = defaultColor;
-      
-    });
+//grid wrapper
+const portfolioWrapper = document.getElementById("portfolio");
 
-    //color change for Product
-    productGrid[0].addEventListener('mouseenter', ()=>{
-        portfolioWrapper.style.backgroundColor = productColor;
-        
-    });
-    productGrid[0].addEventListener('mouseleave', ()=>{
-        portfolioWrapper.style.backgroundColor = defaultColor;
-        
-    });
-    //color change for Fashion
-    fashionGrid[0].addEventListener('mouseenter', ()=>{
-        portfolioWrapper.style.backgroundColor = fashionColor;
-        
-    });
-   fashionGrid[0].addEventListener('mouseleave', ()=>{
-        portfolioWrapper.style.backgroundColor = defaultColor;
-        
-    });
-    
-    //color change for Interior
-    interiorGrid[0].addEventListener('mouseenter', ()=>{
-        portfolioWrapper.style.backgroundColor = interiorColor;
-        
-    });
-    interiorGrid[0].addEventListener('mouseleave', ()=>{
-        portfolioWrapper.style.backgroundColor = defaultColor;
-        
-    });
-    
+// color change for Portait
+portraitGrid[0].addEventListener("mouseenter", () => {
+  portfolioWrapper.style.backgroundColor = portraitColor;
+  portraitText.classList.remove("hide");
+  portraitText.classList.add("show");
+});
+portraitGrid[0].addEventListener("mouseleave", () => {
+  portfolioWrapper.style.backgroundColor = defaultColor;
+  portraitText.classList.remove("show");
+  portraitText.classList.add("hide");
+});
 
-    
-    
+// color change for Auto
+automotiveGrid[0].addEventListener("mouseenter", () => {
+  portfolioWrapper.style.backgroundColor = autoColor;
+  autoText.classList.remove("hide");
+  autoText.classList.add("show");
+});
+
+automotiveGrid[0].addEventListener("mouseleave", () => {
+  portfolioWrapper.style.backgroundColor = defaultColor;
+  autoText.classList.remove("show");
+  autoText.classList.add("hide");
+});
+
+//color change for Product
+productGrid[0].addEventListener("mouseenter", () => {
+  portfolioWrapper.style.backgroundColor = productColor;
+});
+productGrid[0].addEventListener("mouseleave", () => {
+  portfolioWrapper.style.backgroundColor = defaultColor;
+});
+//color change for Fashion
+fashionGrid[0].addEventListener("mouseenter", () => {
+  portfolioWrapper.style.backgroundColor = fashionColor;
+});
+fashionGrid[0].addEventListener("mouseleave", () => {
+  portfolioWrapper.style.backgroundColor = defaultColor;
+});
+
+//color change for Interior
+interiorGrid[0].addEventListener("mouseenter", () => {
+  portfolioWrapper.style.backgroundColor = interiorColor;
+});
+interiorGrid[0].addEventListener("mouseleave", () => {
+  portfolioWrapper.style.backgroundColor = defaultColor;
+});
